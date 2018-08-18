cdc7fde98627d063e601d5b668638872d4bc7084
Sam Smith <samcbr600@gmail.com>
2018-08-18 12:28:59 -0400

    Initial commit

---
+++ start.php
@@ -0,0 +1,32 @@
+ <?php
+/**
+ * Plugin Name: Sam's Helper Function
+ * Plugin URI: https://givewp.com/documentation/developers/how-to-create-custom-form-fields/
+ * Description: This plugin demonstrates adds custom fields to your Give give forms with validation, email functionality, and field data output on the payment record within wp-admin.
+ * Version: 1.0
+ * Author: WordImpress
+ * Author URI: https://givewp.com
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License version 2, as published by the Free Software Foundation.  You may NOT assume
+ * that you can use any other version of the GPL.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * NOTE: This is not a "snippet" but a plugin that you can install and activate. You can put it in a
+ * folder in your /plugins/ directory, or even just drop it directly into the /plugins/ directory
+ * and it will activate like any other plugin.
+ *
+ * DISCLAIMER: This is provided as an EXAMPLE of how to do custom fields for Give. We provide no
+ * guarantees if you put this on a live site. And we do not offer Support for this code at all.
+ * It is simply a free resource for your purposes.
+ */
+
+
+add_action('template_redirect','your_function');
+function your_function() {
+ if ( is_page('test') ) {
+   echo ('here are words');
+ }
+}
