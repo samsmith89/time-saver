840960748c4878c510d4184cbbd2159e53af4f0e
Sam Smith <samcbr600@gmail.com>
2018-08-18 13:05:46 -0400

    getting this back on track againi

--- start.php
+++ start.php
@@ -1,11 +1,10 @@
- <?php
+<?php
 /**
  * Plugin Name: Sam's Helper Function
- * Plugin URI: https://givewp.com/documentation/developers/how-to-create-custom-form-fields/
- * Description: This plugin demonstrates adds custom fields to your Give give forms with validation, email functionality, and field data output on the payment record within wp-admin.
+ * Description: This plugin is to create a workflow for tickets and retunr the values to the user
  * Version: 1.0
- * Author: WordImpress
- * Author URI: https://givewp.com
+ * Author: Sam Smith
+ * Author URI: https://gsamsmith.com
  *
  * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
  * General Public License version 2, as published by the Free Software Foundation.  You may NOT assume
@@ -14,13 +13,7 @@
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *
- * NOTE: This is not a "snippet" but a plugin that you can install and activate. You can put it in a
- * folder in your /plugins/ directory, or even just drop it directly into the /plugins/ directory
- * and it will activate like any other plugin.
  *
- * DISCLAIMER: This is provided as an EXAMPLE of how to do custom fields for Give. We provide no
- * guarantees if you put this on a live site. And we do not offer Support for this code at all.
- * It is simply a free resource for your purposes. this is a test change
  */
 
 
@@ -30,3 +23,26 @@ function your_function() {
    echo ('here are words');
  }
 }
+//Add Test page on activation:
+function install_events_pg(){
+        $new_page_title = 'test';
+        $new_page_content = 'This is your page content that automatically gets inserted into the Events page!';
+        $new_page_template = ''; //ex. template-custom.php. Leave blank if you don't want a custom page template. Look into this as we could fina  wy to create template .phpfile
+        //don't change the code below, unless you know what you're doing
+        $page_check = get_page_by_title($new_page_title);
+        $new_page = array(
+                'post_type' => 'page',
+                'post_title' => $new_page_title,
+                'post_content' => $new_page_content, //This could live in a seprate file
+                'post_status' => 'publish',
+                'post_author' => 1,
+        );
+        if(!isset($page_check->ID)){
+                $new_page_id = wp_insert_post($new_page);
+                if(!empty($new_page_template)){
+                        update_post_meta($new_page_id, '_wp_page_template', $new_page_template);
+                }
+        }
+}//end install_events_pg function to add page to wp on plugin activation
+
+register_activation_hook(__FILE__, 'install_events_pg');
