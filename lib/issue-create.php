b873b765bbb78c97d6f741b333cca90a387eaaee
Sam Smith <samcbr600@gmail.com>
2018-08-19 08:10:11 -0400

    Structure finished enough to work on MySQL

--- lib/issue-create.php
+++ lib/issue-create.php
@@ -2,8 +2,28 @@
 
 //This page houses the form for inputing issues into the database manually
 function issue_create_html() {
- if ( is_page('Manual Input') ) {
-   echo ('This is where the form will live');
+ if ( is_page('Manual Input') ) { ?>
+ <form>
+   <dl>
+     <dt>Status</dt>
+     <dd>
+       <select name="position">
+         <option value="Open">Open</option>
+         <option value="Closed">Closed</option>
+       </select>
+     </dd>
+   </dl>
+   <dl>
+     <dt>Issue Content</dt>
+     <dd>
+       <textarea name="content" cols="60" rows="10"></textarea>
+     </dd>
+   </dl>
+   <div>
+     <input type="submit" value="Submit Issue" />
+   </div>
+ </form>
+ <?php
  }
 }
 add_filter( 'the_content', 'issue_create_html' );
--- lib/repo-page-content.php
+++ lib/repo-page-content.php
@@ -4,8 +4,27 @@
 //hold the tables and the placeholders
 
 function repo_page_content() {
- if ( is_page('Repositories') ) {
-   echo ('here are words');
- }
+ if ( is_page('Repositories') ) { ?>
+   <table class="list">
+  	  <tr>
+        <th>HS Link</th>
+        <th>GH Issue</th>
+        <th>Repo</th>
+        <th>Status</th>
+  	    <th>Release</th>
+  	    <th>Latest Version</th>
+  	    <th>&nbsp;</th>
+        <th>&nbsp;</th>
+  	  </tr>
+        <tr>
+          <td>test</td>
+          <td>test</td>
+          <td>test</td>
+          <td>test</td>
+    	    <td>test</td>
+          <td>test</td>
+    	  </tr>
+  	</table>
+ <?php }
 }
 add_filter( 'the_content', 'repo_page_content' );
